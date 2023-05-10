030b7776be1665d61ab95e4aa10304d8127cee55
jason <jason@Triangulum.dev.0441.design>
2023-05-09 19:30:22 -0700

    refactor moment to luxon

--- bin/common/cachedGet.js
+++ bin/common/cachedGet.js
@@ -1,5 +1,5 @@
 // bin/common/cachedGet.js
-import moment from 'moment-timezone/builds/moment-timezone-with-data-2012-2022.min.js'
+import { DateTime } from 'luxon';
 import _ from 'lodash'
 import axios from 'axios'
 
@@ -7,27 +7,24 @@ const siteCache = {}
 const siteCacheTime = {}
 
 const mondayBetween = pastTime => {
-	const pastMondayStartEpochy = moment()
-		.isoWeekday(1)
-		.hour(0)
-		.minute(0)
-		.second(0)
-		.millisecond(0)
-		.valueOf()
-	const pastTimeEpochy = moment(pastTime).valueOf()
-	return pastTimeEpochy < pastMondayStartEpochy
-}
+	const pastMondayStartEpochy = DateTime.local()
+	  .startOf('week')
+	  .plus({ days: 1 })
+	  .startOf('day')
+	  .toMillis();
+	const pastTimeEpochy = DateTime.fromISO(pastTime).toMillis();
+	return pastTimeEpochy < pastMondayStartEpochy;
+  };
 
-const cachedGet = (url, opt) => {
+const cachedGet = async (url, opt) => {
 	const key = `${url}.${JSON.stringify(opt)}`
 	const time = _.get(siteCacheTime, key, 0)
 	const cacheValid = time && !mondayBetween(time)
-	if (cacheValid) return Promise.resolve(_.get(siteCache, key))
-	return axios.get(url, opt).then(res => {
-		_.set(siteCache, key, res)
-		_.set(siteCacheTime, key, Date.now())
-		return res
-	})
+	if (cacheValid) return _.get(siteCache, key)
+	const res = await axios.get(url, opt);
+	_.set(siteCache, key, res);
+	_.set(siteCacheTime, key, Date.now());
+	return res;
 }
 
 export default cachedGet
